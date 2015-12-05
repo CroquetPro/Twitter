a6197737ba08e52f7799dc782344a3405f64b6a9
Ben Rothman <croquetpro@gmail.com>
2015-12-04 16:23:41 -0800

    buttons functional

--- AjaxTwitter/frontend/follow_toggle.js
+++ AjaxTwitter/frontend/follow_toggle.js
@@ -1,7 +1,48 @@
-function FollowToggle (){
-  this.$el = $('.follow-toggle');
-  this.$userId = this.$el.find("data-user-id");
-  this.$initialFollowState = this.$el.find("data-initial-follow-state");
+function FollowToggle (button){
+  this.$el = $(button);
+  this.$userId = this.$el.data("user-id");
+  this.$initialFollowState = this.$el.data("initial-follow-state");
+  this.$followState = this.$initialFollowState;
+  this.$el.on('click', this.handleClick.bind(this));
+  this.$el.html(this.render());
 }
+
+$.extend(FollowToggle.prototype = {
+  render: function() {
+    if(this.$followState === 'followed') {
+      return "Unfollow!";
+    }
+    else if(this.$followState === 'unfollowed') {
+      return "Follow!";
+    }
+  },
+  handleClick: function(e) {
+    e.preventDefault();
+    var verb, newState;
+
+    if(this.$followState === 'followed') {
+      verb = "DELETE";
+      newState = "unfollowed";
+    }
+    else if(this.$followState === 'unfollowed') {
+      verb = "POST";
+      newState = "followed";
+    }
+
+    $.ajax({
+      url: '/users/' + this.$userId + '/follow',
+      type: verb,
+      dataType: "json",
+      success: function(){
+        this.$followState = newState;
+        this.$el.html(this.render());
+        console.log("Follow status updated.");
+      }.bind(this),
+      error: function(){
+        console.log("I can't even.");
+      }
+    });
+  }
+});
 
 module.exports = FollowToggle;
--- AjaxTwitter/frontend/twitter.js
+++ AjaxTwitter/frontend/twitter.js
@@ -1,1 +1,9 @@
 window.FollowToggle = require ("./follow_toggle.js");
+
+$(document).ready( function() {
+  var buttons = document.querySelectorAll('button.follow-toggle');
+
+  for(var i = 0; i < buttons.length; i++) {
+    new window.FollowToggle(buttons[i]);
+  }
+});
